f1cbaf99f00ea82028a1521eda976f829cad486f
laurentlevasseur3 <31012216+laurentlevasseur3@users.noreply.github.com>
2017-12-07 19:26:23 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,5 +1,30 @@
-package gift1;
-
 public class Account {
+	int balance;
+	int money;
+	String name;
+	public Account(String name)
+	{
+		this.name = name;
+		this.balance = 0;
+	}
+	public void giveMoney(int gift, int people)
+	{
+		money = gift;
+		int leftOvers = money % people;
+		money = money / people;
+		balance = balance - money + leftOvers;
+		
+	}
+	public void receiveMoney(Account account)
+	{
+		account.balance += money;
+	}
+	public int getBalance()
+	{
+		return balance;
+	}
+	public String getName() {
+		return name;
+	}
 
 }
